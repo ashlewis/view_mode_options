c531de6a829b400a20015964ce52580d871c3d6a
ash@s8080.com <ash@s8080.com>
2016-03-15 16:50:59 +0000

    updates

--- js/display_pattern_options.js
+++ js/display_pattern_options.js
@@ -6,21 +6,25 @@
             var
 
                 init = function(){
-                    $('#edit-row-options-view-mode').change(function(){
+                    $('#edit-row-options-view-mode, #edit-build-mode').change(function(){
 
-                        var view_mode = $('#edit-row-options-view-mode').val();
+                        var view_mode = $(this).val();
 
                         $.get(
                             '/display_pattern_options/options/'+ view_mode,
 
                             function(data){
-                                var options = '';
+                                var options = '<option value="default">- Default -</option>';
 
-                                $.each(data, function( key, value ) {
-                                    options += '<option value="'+ key +'">'+ value +'</option>';
-                                });
+                                if (data) {
 
-                                $('#edit-row-options-display-pattern-options').html(options);
+                                    $.each(data, function( key, value ) {
+                                        options += '<option value="'+ key +'">'+ value +'</option>';
+                                    });
+
+                                }
+
+                                $('#edit-row-options-display-pattern-options, #edit-display-pattern-options').html(options);
                             },
 
                             'json'
